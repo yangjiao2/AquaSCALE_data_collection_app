edd17c7ce113e3b962b0d105f97ed4cc7575bc43
YujingXie2015 <yujingxie.dancy@gmail.com>
2016-05-01 16:10:21 -0700

    First commit

---
+++ AddMap2/app/src/main/java/com/example/yujingxie/addmap2/MapsActivity.java
@@ -0,0 +1,58 @@
+package com.example.yujingxie.addmap2;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
+import com.google.android.gms.maps.model.Marker;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+      /*  LatLng irvine = new LatLng(33.69267, -117.81929);
+        Marker marker=mMap.addMarker(new MarkerOptions().position(irvine).title("Marker in Irvine").draggable(true).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).snippet("This is Irvine"));
+        marker.showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(irvine));
+        mMap.setOnMarkerClickListener(new OnMarkerClickListener(){
+            @Override
+            public boolean onMarkerClick(Marker marker){
+
+                return true;
+
+            }
+        });*/
+    }
+}
